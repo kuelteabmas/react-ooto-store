6082d1152065b1d12ae211e42e8198d4fa34fddf
Muhamed Fall <fallmuhamed@gmail.com>
2018-12-23 02:47:48 -0500

    added files
    Added css files.
    Add Header. Inventory, Order components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import StorePicker from "./components/StorePicker";
+import Header from "./components/Header";
+import Inventory from "./components/Inventory";
+import Order from "./components/Order";
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Daily"/>
+        </div>
+        <Inventory />
+        <Order />
       </div>
     );
   }
